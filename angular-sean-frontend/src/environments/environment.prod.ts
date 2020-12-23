8c02e5441db19fa0f7c58e835611eb113bb64d70
Thomas E. Kremer <68854175+tkremer72@users.noreply.github.com>
2020-12-22 21:10:29 -0500

    Delete environment.prod.ts

--- angular-sean-frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  adminApi: 'http://localhost:4000/admins/',
-  authsApi: 'http://localhost:4000/auths/',
-  usersApi: 'http://localhost:4000/users/',
-  blogsApi: 'http://localhost:4000/blogs/'
-};
